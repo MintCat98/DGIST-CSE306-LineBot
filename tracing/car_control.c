b047a5571ba17f5eb9f1adf557698ae492094aae
MintCat <presidentmc9898@gmail.com>
2024-05-27 23:58:30 +0900

    [fix] Entire code structures

--- tracing/car_control.c
+++ tracing/car_control.c
@@ -1,75 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <wiringPi.h>
-#include <wiringPiI2C.h>
+#include <unistd.h>
 #include "car_control.h"
 
 #define CAR_ADDRESS 0x16
 
 int fd;
 
-void setup_i2c() {
-    fd = wiringPiI2CSetup(CAR_ADDRESS);
-    if (fd == -1) {
-        printf("Failed to initialize I2C\n");
+void write_block_data(int i2c_addr, unsigned char* data, int length) {
+    int new_len = length + 1;
+    unsigned char copy[new_len];
+    copy[0] = i2c_addr;
+    for (size_t i = 0; i < length; i++)
+    {
+        temp[i + 1] = data[i];
+    }
+    if (write(fd, temp, new_len) != new_len) {
+        printf("[I2C Error] Failed to write.\n");
         exit(1);
     }
 }
 
-void write_block_data(int i2c_addr, unsigned char* data, int length) {
-    for (int i = 0; i < length; ++i) {
-        int result = wiringPiI2CWrite(fd, data[i]);
-        if (result == -1) {
-            printf("Failed to write data byte %d\n", i);
-            exit(1);
-        }
-    }
-}
-
 void Ctrl_Car(int l_dir, int l_speed, int r_dir, int r_speed) {
+    int i2c_addr;
     unsigned char data[] = { l_dir, l_speed, r_dir, r_speed };
-    write_block_data(CAR_ADDRESS, data, 4);
+    write_block_data(i2c_addr, data, 4);
 }
 
-void Control_Car(int speed1, int speed2) {
+void Run_Car(int speed1, int speed2) {
     int dir1 = speed1 < 0 ? 0 : 1;
     int dir2 = speed2 < 0 ? 0 : 1;
     Ctrl_Car(dir1, abs(speed1), dir2, abs(speed2));
 }
-
-void Car_Run(int speed1, int speed2) {
-    Ctrl_Car(1, speed1, 1, speed2);
-}
-
-void Car_Stop() {
-    unsigned char data[] = { 0x02, 0x00 };
-    write_block_data(CAR_ADDRESS, data, 2);
-}
-
-void Car_Back(int speed1, int speed2) {
-    Ctrl_Car(0, speed1, 0, speed2);
-}
-
-void Car_Left(int speed1, int speed2) {
-    Ctrl_Car(0, speed1, 1, speed2);
-}
-
-void Car_Right(int speed1, int speed2) {
-    Ctrl_Car(1, speed1, 0, speed2);
-}
-
-void Car_Spin_Left(int speed1, int speed2) {
-    Ctrl_Car(0, speed1, 1, speed2);
-}
-
-void Car_Spin_Right(int speed1, int speed2) {
-    Ctrl_Car(1, speed1, 0, speed2);
-}
-
-void Ctrl_Servo(int id, int angle) {
-    if (angle < 0) angle = 0;
-    else if (angle > 180) angle = 180;
-    unsigned char data[] = { id, angle };
-    write_block_data(CAR_ADDRESS, data, 2);
-}
-
--- tracing/car_control.h
+++ tracing/car_control.h
@@ -16,22 +16,6 @@ void write_block_data(int i2c_addr, unsigned char* data, int length);
 
 void Ctrl_Car(int l_dir, int l_speed, int r_dir, int r_speed);
 
-void Control_Car(int speed1, int speed2);
-
-void Car_Run(int speed1, int speed2);
-
-void Car_Stop();
-
-void Car_Back(int speed1, int speed2);
-
-void Car_Left(int speed1, int speed2);
-
-void Car_Right(int speed1, int speed2);
-
-void Car_Spin_Left(int speed1, int speed2);
-
-void Car_Spin_Right(int speed1, int speed2);
-
-void Ctrl_Servo(int id, int angle);
+void Run_Car(int speed1, int speed2);
 
 #endif // CAR_CONTROL_H

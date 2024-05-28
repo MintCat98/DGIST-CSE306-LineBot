0155a339218dbe28f830bbf27a16c21a835c4d74
Sangwoo Jeon <jharry0503@gmail.com>
2024-05-28 23:04:14 +0900

    wrong definition of sensor deleted

--- tracing/car_tracking.h
+++ tracing/car_tracking.h
@@ -1,11 +1,6 @@
 #ifndef TRACKING_FUNCTION_H
 #define TRACKING_FUNCTION_H
 
-#define Tracking_Left1 0
-#define Tracking_Left2 1
-#define Tracking_Right1 2
-#define Tracking_Right2 3
-
 void setup();
 void tracking_function();
 
